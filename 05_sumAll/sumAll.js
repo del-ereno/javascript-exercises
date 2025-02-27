3a3cd1b37e1b54a17583ca1b5d654c5c8670fae1
del-ereno <delereno2@gmail.com>
2025-02-27 20:14:17 +1300

    js exercise 5 complete

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(num1,num2) {
+    var sum = 0;
+    if (Number.isInteger(num1) && Number.isInteger(num2)){
+        if(num1 >= 0 && num2 >= 0){
+            if (num1 > num2){
+                let num3 = num1;
+                num1 = num2;
+                num2 = num3;
+            }
+            for(let i = num2;i>=num1;i--){
+                sum += i;
+            }
+            return sum;
+        }
+    }
+    return "ERROR";
 };
 
 // Do not edit below this line
