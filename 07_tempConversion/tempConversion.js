1b4c5dc72f792c4e1b7d79c0fc4c15c19afa056e
del-ereno <delereno2@gmail.com>
2025-02-27 20:29:30 +1300

    js ex 7 complete mashallah

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fhVal) {
+  return (Number(((fhVal - 32) * (5/9)).toFixed(1)));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celVal) {
+  return (Number((celVal * (9/5) + 32).toFixed(1)));
 };
 
 // Do not edit below this line
