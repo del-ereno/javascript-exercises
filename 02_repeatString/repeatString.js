27f5652c4210133346847cbb084ced3f6d812c11
del-ereno <delereno2@gmail.com>
2025-02-27 19:18:22 +1300

    ex 1-3 for js finished

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
 
+    var stringSum = "";
+    if (num > 0){
+        for(let i = 0; i < num;i++){
+            stringSum = stringSum.concat(string);
+        }
+    }
+    else if (num < 0){
+        return("ERROR")
+    }
+    return stringSum;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    var reversedString = "";
+    var stringArray = (string.split("")).reverse();
 
+    for(let i = 0; i < stringArray.length; i++){
+        reversedString = reversedString.concat(stringArray[i]);
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
