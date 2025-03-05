3280629bca0df2f8f47a10d75f0676bde64b9d5c
del-ereno <delereno2@gmail.com>
2025-03-05 13:45:16 +1300

    exercise finished

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,18 @@
-const palindromes = function () {
+const palindromes = function (palString) {
+    palString = palString.replace(/[\s.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").toLowerCase();
+    palArray = palString.split("");
+    let midIdx = Math.round(palArray.length / 2);
+    var a = palArray.slice(0, midIdx-1);
+
+    //for even length string splitting
+    if (palArray.length % 2 == 0){
+        midIdx += 1;
+    }
+
+    var b = palArray.slice(midIdx).reverse();
+    let res = a.join() == b.join();
+
+    return res;
 
 };
 
