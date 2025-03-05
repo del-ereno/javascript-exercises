e11737f67d9bbccdfeb8b80eab339cc842107ff2
del-ereno <delereno2@gmail.com>
2025-03-05 13:58:46 +1300

    exercise finishedw

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    num = Number(num);
+    if (num == 0){
+        return 0;
+    }
+    else if (num < 0){
+        return "OOPS";
+    }
+    let prevFib = 0;
+    let fibNum = 1;
+    for(let i = 1; i < num; i++){
+        let tempFib = fibNum + prevFib;
+        prevFib = fibNum;
+        fibNum = tempFib; 
+    }
+    return fibNum;
 };
 
 // Do not edit below this line
