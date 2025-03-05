191556651914af72df35933458c1b1bf784c50d4
del-ereno <delereno2@gmail.com>
2025-03-05 13:06:22 +1300

    finished exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1+num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1-num2;
 };
 
-const sum = function() {
-	
+const sum = function(numList) {
+	return numList.reduce((total, currentItem) => {return total + currentItem;},0);
 };
 
-const multiply = function() {
-
+const multiply = function(numList) {
+  return numList.reduce((total, currentItem) => {return total * currentItem;},1);
 };
 
-const power = function() {
-	
+const power = function(num1,num2) {
+	return (Math.pow(num1,num2));
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num === 0){
+    return 1;
+  }
+  else{
+    let numList = []
+    while (num != 0){
+      numList.push(num);
+      num -=1;
+    }
+    return numList.reduce((total, currentItem) => {return total * currentItem;},1);
+  }
 };
 
 // Do not edit below this line
