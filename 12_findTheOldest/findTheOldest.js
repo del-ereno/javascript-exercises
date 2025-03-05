69d7127c35a94416df4cf01e25aa6f80db43111a
del-ereno <delereno2@gmail.com>
2025-03-05 14:31:53 +1300

    exercise completed

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    
+    function findAge(person){
+        if (person.yearOfDeath === undefined) {
+            person.yearOfDeath = 2025;
+        }
+        return (person.yearOfDeath - person.yearOfBirth); 
+    }
+    const ageMap = arr.map(findAge);
+    const oldestIdx = ageMap.indexOf(Math.max(...ageMap));
+    return arr[oldestIdx];
+    
 
 };
 
